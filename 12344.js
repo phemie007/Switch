62230dccedda1d27137cbe79a6f99211406a2efd
phemie007 <ogunsolaoluwafemi@ymail.com>
2019-08-20 13:09:37 -0700

    complete on and off switch codes

---
+++ 12344.js
@@ -0,0 +1,13 @@
+function switchon(){
+	dd.style.display = "block";
+	cc.style.display = "none";
+	bb.style.display = "block";
+	aa.style.display ="none";
+}
+
+function switchoff(){
+	dd.style.display = "none";
+	cc.style.display = "block";
+	bb.style.display = "none";
+	aa.style.display ="block";
+}
